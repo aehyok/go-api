ec3cad601d9ceb0c558bc7da776ea02881a8780c
刘启明 <455043818@qq.com>
2021-12-02 17:41:35 +0800

    commit(查询不建结构返回数据列表)

--- hello.go
+++ hello.go
@@ -39,15 +39,16 @@ func updateRecord(Db *sqlx.DB) {
 func main() {
 	var Db *sqlx.DB = connectMysql()
 	defer Db.Close()
-	fmt.Printf("insert success\n")
-	addRecord(Db)
-	updateRecord(Db)
-	deleteRecord(Db)
+	selectRecord(Db)
+	fmt.Printf("select success\n")
+	// addRecord(Db)
+	// updateRecord(Db)
+	// deleteRecord(Db)
 }
 
 func addRecord(Db *sqlx.DB) {
 	for i := 0; i < 2; i++ {
-		result, err := Db.Exec("insert into user (id,account,nickName,password) values(5,'111', '222', '3333')")
+		result, err := Db.Exec("insert into user (id,account,nickName,password) values(7,'111', '222', '3333')")
 		if err != nil {
 			fmt.Printf("data insert faied, error:[%v]", err.Error())
 			return
@@ -55,6 +56,38 @@ func addRecord(Db *sqlx.DB) {
 		id, _ := result.LastInsertId()
 		fmt.Printf("insert success, last id:[%d]\n", id)
 	}
+}
+
+func selectRecord(Db *sqlx.DB) {
+	rows, err := Db.Query(" select * from  user")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+
+	//列出所有查询结果的字段名
+	cols, _ := rows.Columns()
+
+	//values是每个列的值，这里获取到byte里
+	values := make([][]byte, len(cols))
+	//query.Scan的参数，因为每次查询出来的列是不定长的，用len(cols)定住当次查询的长度
+	scans := make([]interface{}, len(cols))
+	//让每一行数据都填充到[][]byte里面
+	for i := range values {
+		scans[i] = &values[i]
+	}
+	// res := make([]map[string]string, 0)
+	for rows.Next() {
+		_ = rows.Scan(scans...)
+		row := make(map[string]string)
+		for k, v := range values { //每行数据是放在values里面，现在把它挪到row里
+			key := cols[k]
+			row[key] = string(v)
+		}
+		fmt.Println(row)
+		// res = append(res, row)
+	}
+	// fmt.Println(res)
 }
 
 func deleteRecord(Db *sqlx.DB) {
